ef8ab8dae2c453760283776df7db675f793e3793
Wzp-2008 <18758003180@163.com>
2019-09-29 15:39:05 +0800

    python_backup

--- pc/1.py
+++ pc/1.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://fanyi.baidu.com/?aldtype=16047#en/zh/Zoom"
+url = "https://www.bilibili.com/video/av58329669"
 HTML = requests.get(url)
 HTML.encoding = "utf-8"
 HTML = HTML.text
 soup = BeautifulSoup(HTML)
 soup_t = soup.prettify()
-p = soup.find_all("strong")
+p = soup.find_all("span")
 print(p)
